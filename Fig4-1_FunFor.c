2a3ca93dfcae362b09f5e7488952784a89d94d40
CoodersSystems-AT <cooderssystems.development@gmail.com>
2015-06-04 23:45:05 -0300

    Create Fig4-1_FunFor.c

---
+++ Fig4-1_FunFor.c
@@ -0,0 +1,14 @@
+/* Fig4-1_FunFor.c
+Muestra de la Funcion for
+Para Compilar # gcc Fig4-1_FunFor.c -o Nombre_ejecutable */
+#include
+
+int main () {
+
+int Contador;
+
+for(Contador = 1; Contador <= 10; Contador++){
+printf("%d\n", Contador);
+}
+return 0;
+}
